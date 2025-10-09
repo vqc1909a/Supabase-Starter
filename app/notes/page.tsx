e3ffd5554e58c3809d7e97633b1f949a393463e0
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-10-09 12:38:06 -0500

    feat: implement async data fetching for notes in page component

---
+++ app/notes/page.tsx
@@ -0,0 +1,37 @@
+import {createClient} from "@/lib/supabase/server";
+
+// "use client";
+
+// import {createClient} from "@/lib/supabase/client";
+// import {useEffect, useState} from "react";
+
+// export default function Page() {
+// 	const [notes, setNotes] = useState<any[] | null>(null);
+// 	const supabase = createClient();
+
+// 	useEffect(() => {
+// 		const getData = async () => {
+// 			const {data} = await supabase.from("notes").select();
+// 			setNotes(data);
+// 		};
+// 		getData();
+// 	}, []);
+
+// 	return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+// }
+
+
+export default async function Page() {
+
+  const supabase = await createClient();
+  const {data: notes, error} = await supabase.from("notes").select();
+
+
+  if(error){
+    console.log({
+      error, 
+    })
+    throw new Error(error.message); 
+  }
+	return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+}
