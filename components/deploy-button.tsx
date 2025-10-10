ca07422140a9ebab5b3d7ce5094dfb44e4b01f61
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-10-09 20:18:40 -0500

    fix: update demo URL and image in DeployButton component for accuracy

--- components/deploy-button.tsx
+++ components/deploy-button.tsx
@@ -5,7 +5,7 @@ export function DeployButton() {
   return (
 		<>
 			<Link
-				href="https://vercel.com/new/clone?repository-url=https://github.com/vqc1909a/Supabase-Starter&project-name=nextjs-with-supabase&repository-name=nextjs-with-supabase&demo-title=nextjs-with-supabase&demo-description=This starter configures Supabase Auth to use cookies, making the user's session available throughout the entire Next.js app - Client Components, Server Components, Route Handlers, Server Actions and Middleware.&demo-url=https://demo-nextjs-with-supabase.vercel.app/&external-id=https://github.com/vercel/next.js/tree/canary/examples/with-supabase&demo-image=https://demo-nextjs-with-supabase.vercel.app/opengraph-image.png"
+				href="https://vercel.com/new/clone?repository-url=https://github.com/vqc1909a/Supabase-Starter&project-name=nextjs-with-supabase&repository-name=nextjs-with-supabase&demo-title=nextjs-with-supabase&demo-description=This starter configures Supabase Auth to use cookies, making the user's session available throughout the entire Next.js app - Client Components, Server Components, Route Handlers, Server Actions and Middleware.&demo-url=https://supabase-starter-teal.vercel.app/&demo-image=https://user-images.githubusercontent.com/70828596/236632861-4f0e6f1c-3d3a-4e2b-8f7d-3e5f1d512bcb.png"
 				target="_blank"
 			>
 				<Button className="flex items-center gap-2" size="sm">
